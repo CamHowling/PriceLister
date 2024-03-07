f47aee9ddfcea8532cea20d4c2dfa8b4c0265404
CamHowling <>
2024-03-07 14:32:21 +1100

    cleanup program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using buildxact_supplies.Services.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Linq;
 
 namespace SuppliesPriceLister
 {
@@ -25,28 +26,25 @@ namespace SuppliesPriceLister
 
             using (IHost host = builder.Build())
             {
-                //Consider adding an intermediate service to demonstrate a more typical approach to DI
                 //in the very least can method extract...
                 using (IServiceScope scope = host.Services.CreateScope())
                 {
                     IJsonService jsonService = scope.ServiceProvider.GetRequiredService<IJsonService>();
                     ICsvService csvService = scope.ServiceProvider.GetRequiredService<ICsvService>();
                     var humphriesSupplies = csvService.GetSuppliesFromHumphries();
-                    
-                    foreach(var supply in humphriesSupplies)
+
+                    //need to replace supplies selected
+                    var sortedSupplies = humphriesSupplies.OrderByDescending(supply => supply.Price);
+                    foreach(var supply in sortedSupplies)
                     {
                         Console.WriteLine(supply.ToString());
+                        Console.WriteLine();
                     }
-                    //structure:
-                    //process humphries
-                    //retrieve supplies
+
                     //process megacorp
                     //  This includes ensuring that all guids are unique - if they are not, need to adjust...
                     //  megacorp needs to be adjusted from USD to AUD
                     //retrieve megacorp
-
-                    //sort all by price
-                    //print with newline between; fields: Id, Item Name, and Price
                 }
 
                 runHost(host);
